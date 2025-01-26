649ed1fef25cadcdf34209a628843be316e667a0
IMarcusDev <escobarmarcos511@gmail.com>
2025-01-26 02:13:50 -0500

    Hotfix Alquiler v0.3 + contactos

--- 2P/scripts/updateStater.js
+++ 2P/scripts/updateStater.js
@@ -5,3 +5,15 @@ function redirectToLibrosPage() {
 function redirectToContactosPage(){
     window.top.location.href = "./contacto.html";
 }
+
+function transferState() {
+    const state = document.getElementById("modoOscuro").checked;
+
+    const iframe = document.getElementById("screen");
+    const iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+
+    const iframeCheckbox = iframeDocument.getElementById("modoOscuro");
+    if (iframeCheckbox) {
+        iframeCheckbox.checked = state;
+    }
+}
